4fb62bdeb1c15d37759a1aba0bc2e7f69cf22b64
marwachafii <36714386+marwachafii@users.noreply.github.com>
2019-08-19 10:57:17 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -127,12 +127,12 @@ app.controller('controller', ['$scope', '$rootScope', '$location', function ($sc
 		{
 			title: '[C21] A Novel Modulation for IoT : PSK-LoRa',
 			link: '#',
-			body: 'Bomfin R, Chafii M, Fettweis G. IEEE VTC-sping (Kuala Lumpur, Hawaii, Malaisia, Mai 2019).'
+			body: 'Bomfin R, Chafii M, Fettweis G. IEEE VTC-sping (Kuala Lumpur, Malaisia, Mai 2019).'
 		},
 		{
 			title: '[C20] Precoded-OFDM within GFDM Framework',
 			link: '#',
-			body: 'Nimr A, Chafii M, Fettweis G. IEEE VTC-sping (Kuala Lumpur, Hawaii, Malaisia, Mai 2019).'
+			body: 'Nimr A, Chafii M, Fettweis G. IEEE VTC-sping (Kuala Lumpur, Malaisia, Mai 2019).'
 		},
 		{
 			title: '[C19] Low-Complexity Iterative Receiver for Othogonal Chirp Division Multiplexing',
