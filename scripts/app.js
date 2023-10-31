c3ea8d3f52c2f6a13ebd27425f9a647a0d12d17a
marwachafii <36714386+marwachafii@users.noreply.github.com>
2023-10-31 17:49:26 +0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -70,6 +70,10 @@ app.controller('controller', ['$scope', '$rootScope', '$location', function ($sc
 
 	$scope.journals = [
 		{
+			title: '[J40] Layered Chirp Spread Spectrum Modulations for LPWANs',
+			body: 'Azim A, Bazzi A, Bomfin R, Shubair R, Chafii M, IEEE Transactions on Communications, 2023'
+		},
+		{
 			title: '[J39] Sparse-DFT and WHT Precoding with Iterative Detection for Highly Frequency-Selective Channels',
 			body: 'Bomfin R, Chafii M, IEEE Transactions on Wireless Communications, 2023'
 		},
